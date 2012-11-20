05008f0a294328a32e63f46d27cb2ed71c529997
Aaron Meier <aaron@quirler.com>
2012-11-19 20:18:16 -0700

    Added mobile theme

---
+++ src/templates/theme-light-mobile/theme.py
@@ -0,0 +1,11 @@
+theme_data = {
+    'label': 'Light Mobile',
+    'name': 'theme-light-mobile',
+    'author': 'Aaron Meier',
+    'version': '1.1.6',
+    'valid_html': True,
+    'valid_css': True,
+    'requires_static': True,
+    'description': '''A fluid mobile-supported valid HTML5/CSS theme''',
+}
+    
